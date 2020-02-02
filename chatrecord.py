708d3ae94b285d62d2a1466005a43501bfc06d1a
Samuel Tijani <omalitijani@gmail.com>
2020-02-02 07:27:52 +0100

    Add chat record

---
+++ chatrecord.py
@@ -0,0 +1,17 @@
+"""
+Author: Samuel Tijani
+"""
+
+class ChatRecord(object):
+    
+	def __init__(self):
+		self.data = []
+
+	def add(self, s):
+		self.data.append(s)
+
+	def __str__(self):
+		if len(self.data) == 0:
+			return 'No messages yet!'
+		else:
+			return '\n'.join(self.data)
